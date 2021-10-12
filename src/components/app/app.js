26deb0ece61d391a12a0b359a0e2452b2411ee1b
Sergey <sergekotov95@gmail.com>
2021-10-12 07:56:53 +0300

    Added uuid, functionality to add and delete employees
    Now there is a possible to add and delete employees.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,6 @@
+import { Component } from 'react';
+import {v4 as uuidv4} from 'uuid';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,25 +9,66 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-    const data = [
-        {name: 'John', salary: 800, increase: false, id:1},
-        {name: 'Alex', salary: 5000, increase: true, id:2},
-        {name: 'Carl', salary: 1000, increase: false, id:3}
-    ];
-    return (
-        <div className="app">
-            <AppInfo/>
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John', salary: 800, increase: false, id:1},
+                {name: 'Alex', salary: 5000, increase: true, id:2},
+                {name: 'Carl', salary: 1000, increase: false, id:3}
+            ]
+        }
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex(elem => elem.id === id);
+            
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
+            
+            // const newArr = [...before, ...after];
+
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+            
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false, 
+            id: uuidv4()
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+    
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App; 
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -18,7 +18,7 @@ class EmployeesListItem extends Component{
     }
 
     render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase, like} = this.state; 
 
         let classNames = 'list-group-item d-flex justify-content-between';
@@ -41,7 +41,8 @@ class EmployeesListItem extends Component{
                     </button>
 
                     <button type="button"
-                            className="btn-trash btn-sm ">
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
                         <i className="fas fa-trash"></i>
                     </button>
                     <i className="fas fa-star"></i>
